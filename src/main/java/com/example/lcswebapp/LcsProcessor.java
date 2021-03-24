df756575745b402101dfc13f694a37f2dee90dc8
aazeezee <djabzeez97@live.com>
2021-03-24 08:11:41 -0400

    Attempting to improve current polynomial time complexity of O(N^2 * (R * S)), where N is the number of strings in the input JSON and R and S are the lengths of the two longest strings

--- src/main/java/com/example/lcswebapp/LcsProcessor.java
+++ src/main/java/com/example/lcswebapp/LcsProcessor.java
@@ -10,18 +10,100 @@ public class LcsProcessor {
      * @return A List of Strings containing the longest common substrings
      */
     public List<String> handler(List<String> values) {
-        Set<String> lcsSet = new HashSet<>(0);
-
         if (values.size() == 1) {
             return values;
         } else {
-            for (int str1 = 0; str1 < values.size() - 1; str1++) {
-                for (int str2 = str1 + 1; str2 < values.size(); str2++) {
-                    lcsSet.addAll(getLongestCommonSubstrings(values.get(str1), values.get(str2)));
+            List<String> lcsList = new ArrayList<String>(getLongestCommonSubstrings(values));
+            Collections.sort(lcsList);
+            return lcsList;
+        }
+    }
+
+    /**
+     * Finds all substrings of every String in the passed-in Set of Strings,
+     * then stores these substrings as keys in a Map, where the value of each key is a
+     * Set of Strings. These Sets of String values will each be a Set of the original String(s)
+     * to which the key is a substring of.
+     * For example: "Sonic" is a substring of both "Sonic Adventure" and "Sonic & Knuckles",
+     * and would therefore lead to an entry in the Map where the key-value pair is:
+     * Key = "Sonic", Value = {"Sonic Adventure", "Sonic & Knuckles"}
+     * @param originalStrings
+     * @return
+     */
+    public Set<String> getLongestCommonSubstrings(List<String> originalStrings) {
+        Map<String, Set<String>> allSubstrings = new HashMap<String, Set<String>>();
+        Set<String> currentLcsSet = new HashSet<String>();
+        for (int origIdx = 0; origIdx < originalStrings.size(); origIdx++) {
+            String original = originalStrings.get(origIdx);
+            for (int leftIdx = 0; leftIdx < original.length(); leftIdx++) {
+                for (int rightIdx = leftIdx; rightIdx < original.length(); rightIdx++) {
+                    String subStr = original.substring(leftIdx, rightIdx + 1);
+                    if (!allSubstrings.containsKey(subStr)) {
+                        allSubstrings.put(
+                                subStr,
+                                new HashSet<String>(Arrays.asList(original))
+                        );
+                    } else {
+                        allSubstrings.get(subStr).add(original);
+                    }
+                }
+            }
+            if (origIdx >= 1) {
+                currentLcsSet = truncateAllSubstringsMap(allSubstrings);
+            }
+        }
+        return retainMax(currentLcsSet, 0);
+    }
+
+    /**
+     * Method to optimize space complexity by truncating the "allSubstrings" Map.
+     * Takes in the "allSubstrings" Map and finds all keys where the value is a Set with
+     * a size greater than 1. Then, among those keys, finds the key(s) with the longest length
+     * (the current LCS(s)). Finally, removes all keys whose lengths are less than the current LCS(s)
+     * and returns that Set of substring keys
+     * @param allSubstrings
+     * @return The current Set of LCS(s) found so far
+     */
+    public Set<String> truncateAllSubstringsMap(Map<String, Set<String>> allSubstrings) {
+        Map<String, Set<String>> truncatedSubStrMap = new HashMap<String, Set<String>>();
+        Set<String> currentLcsSet = new HashSet<String>();
+        Set<String> keysStillInPlay = new HashSet<String>();
+        // Finding all the keys that belong to more than one String in the original List of String values
+        // (meaning they are mapped to a Set with a size greater than 1).
+        // In other words, finding common substrings.
+        int maxLcsLength = 0;
+        for (String subStr : allSubstrings.keySet()) {
+            if (allSubstrings.get(subStr).size() > 1) {
+                currentLcsSet.add(subStr);
+                if (maxLcsLength < subStr.length()) {
+                    maxLcsLength = subStr.length();
                 }
             }
         }
-        return new ArrayList<String>(retainMax(lcsSet));
+        // If we haven't found any common substrings, we don't want to remove anything from the map yet
+        if (currentLcsSet.size() != 0) {
+            // Retrieving all substrings from the current map that are the same length or longer than
+            // the current LCS(s)
+            for (String subStr : allSubstrings.keySet()) {
+                if (subStr.length() >= maxLcsLength) {
+                    keysStillInPlay.add(subStr);
+                }
+            }
+            // Removing all keys whose lengths are less than the current LCS(s) by leaving them out
+            // of the truncated Map.
+            // We don't care about substrings that are shorter than the current LCS(s), no matter
+            // how many times they appear, as they can never be the LONGEST common substring
+            for (String key : allSubstrings.keySet()) {
+                if (keysStillInPlay.contains(key)) {
+                    truncatedSubStrMap.put(key, allSubstrings.get(key));
+                }
+            }
+            allSubstrings.clear();
+            allSubstrings.putAll(truncatedSubStrMap);
+            return currentLcsSet;
+        } else {
+            return retainMax(allSubstrings.keySet(), 0);
+        }
     }
 
     /**
@@ -30,7 +112,7 @@ public class LcsProcessor {
      * @param str2
      * @return
      */
-    public Set<String> getLongestCommonSubstrings(String str1, String str2) {
+    public Set<String> oldGetLCSs(String str1, String str2) {
         int max = 0;
         // The matrix created by the cross-section of the two parameter strings
         int[][] matrix = new int[str1.length()][str2.length()];
@@ -64,33 +146,19 @@ public class LcsProcessor {
     /**
      * Takes in a Set of Strings and removes all Strings except for the longest one(s)
      * @param stringSet
-     * @return A new Set of Strings containing only the longest String(s) from the passed in Set
-     */
-    public Set<String> retainMax(Set<String> stringSet) {
-        Set<String> maxSet = new HashSet<>(0);
-        int max = 0;
-        for (String str : stringSet) {
-            if (max < str.length()) {
-                max = str.length();
-            }
-        }
-
-        for (String str : stringSet) {
-            if (str.length() == max) {
-                maxSet.add(str);
-            }
-        }
-        return maxSet;
-    }
-
-    /**
-     * Overloaded version of retainMax() with max value passed in
-     * @param stringSet
-     * @param max
+     * @param max Can be passed in as 0 when the max length is unknown
      * @return A new Set of Strings containing only the longest String(s) from the passed in Set
      */
     public Set<String> retainMax(Set<String> stringSet, int max) {
-        Set<String> maxSet = new HashSet<>(0);
+        Set<String> maxSet = new HashSet<String>();
+        if (max == 0) {
+            for (String str : stringSet) {
+                if (max < str.length()) {
+                    max = str.length();
+                }
+            }
+        }
+
         for (String str : stringSet) {
             if (str.length() == max) {
                 maxSet.add(str);
--- src/test/java/com/example/lcswebapp/LcsProcessorTest.java
+++ src/test/java/com/example/lcswebapp/LcsProcessorTest.java
@@ -8,19 +8,19 @@ public class LcsProcessorTest {
     private LcsProcessor lcsProcessor = new LcsProcessor();
 
     @Test
-    public void testGetLongestCommonSubstring() {
+    public void testOldGetLongestCommonSubstring() {
         Set<String> testResultSet = new HashSet<>(0);
         testResultSet.add(" Adventure");
         String str1 = "Sonic 2 Battle Adventure";
         String str2 = "Sonic DX! Adventure";
-        Set<String> actualResultSet = lcsProcessor.getLongestCommonSubstrings(str1, str2);
+        Set<String> actualResultSet = lcsProcessor.oldGetLCSs(str1, str2);
 
         assertEquals(testResultSet.size(), actualResultSet.size());
         assertTrue(actualResultSet.contains(" Adventure"));
     }
 
     @Test
-    public void testGetLongestCommonSubstringWithMultipleCommonSubstrings() {
+    public void testOldGetLongestCommonSubstringWithMultipleCommonSubstrings() {
         Set<String> testResultSet = new HashSet<>(0);
         testResultSet.add("*Adventure *");
         testResultSet.add("* Dreamcast*");
@@ -29,12 +29,80 @@ public class LcsProcessorTest {
                 "then remade in 2003 for the Nintendo* GameCube *as \"Sonic *Adventure *DX\"";
         String str2 = "The sequel, \"*Adventure *2\" was also first launched on the* Dreamcast* in 2001, " +
                 "then ported in an enhanced version for Nintendo's* GameCube *in 2002 as \"Adventure 2: Battle\"";
-        Set<String> actualResultSet = lcsProcessor.getLongestCommonSubstrings(str1, str2);
+        Set<String> actualResultSet = lcsProcessor.oldGetLCSs(str1, str2);
 
         assertEquals(testResultSet.size(), actualResultSet.size());
         assertTrue(actualResultSet.contains("*Adventure *"));
         assertTrue(actualResultSet.contains("* Dreamcast*"));
         assertTrue(actualResultSet.contains("* GameCube *"));
+    }
+
+    @Test
+    public void testGetLcs() {
+        Set<String> testResultSet = new HashSet<>(0);
+        testResultSet.add(" Adventure");
+        String str1 = "Sonic 2 Battle Adventure";
+        String str2 = "Sonic DX! Adventure";
+        List<String> testInputList = new ArrayList<String>(Arrays.asList(str1, str2));
+        Set<String> actualResultSet = lcsProcessor.getLongestCommonSubstrings(testInputList);
+
+        assertEquals(testResultSet.size(), actualResultSet.size());
+        assertTrue(actualResultSet.contains(" Adventure"));
+    }
+
+    @Test
+    public void testGetLcsWithMultipleCommonSubstrings() {
+        Set<String> testResultSet = new HashSet<>(0);
+        testResultSet.add("*Adventure *");
+        testResultSet.add("* Dreamcast*");
+        testResultSet.add("* GameCube *");
+        String str1 = "\"Sonic Adventure\" was released in 1999 on the Sega* Dreamcast*, " +
+                "then remade in 2003 for the Nintendo* GameCube *as \"Sonic *Adventure *DX\"";
+        String str2 = "The sequel, \"*Adventure *2\" was also first launched on the* Dreamcast* in 2001, " +
+                "then ported in an enhanced version for Nintendo's* GameCube *in 2002 as \"Adventure 2: Battle\"";
+        String str3 = "Will there be an '*Adventure *3'? No one really knows";
+        List<String> testInputList = new ArrayList<String>(Arrays.asList(str1, str2, str3));
+        Set<String> actualResultSet = lcsProcessor.getLongestCommonSubstrings(testInputList);
+
+        assertEquals(testResultSet.size(), actualResultSet.size());
+        assertTrue(actualResultSet.contains("*Adventure *"));
+        assertTrue(actualResultSet.contains("* Dreamcast*"));
+        assertTrue(actualResultSet.contains("* GameCube *"));
+    }
+
+    @Test
+    public void testGetLcsWithNoCommonSubstrings() {
+        Set<String> testResultSet = new HashSet<>(0);
+        testResultSet.add("56789_@$#&");
+        String str1 = "ABCDEFG";
+        String str2 = "HIJKLMN";
+        String str3 = "OPQRSTUV";
+        String str4 = "WXYZ?!";
+        String str5 = "01234";
+        String str6 = "56789_@$#&";
+        List<String> testInputList = new ArrayList<String>(Arrays.asList(str1, str2, str3, str4, str5, str6));
+        Set<String> actualResultSet = lcsProcessor.getLongestCommonSubstrings(testInputList);
+
+        assertEquals(testResultSet.size(), actualResultSet.size());
+        assertTrue(actualResultSet.contains("56789_@$#&"));
+    }
+
+    @Test
+    public void testGetLcsWithNoCommonSubstringsMultipleLongest() {
+        Set<String> testResultSet = new HashSet<>(0);
+        testResultSet.add("RSTUVWXYZ?!");
+        testResultSet.add("456789_@$#&");
+        String str1 = "ABCDEFG";
+        String str2 = "HIJKLMNOPQ";
+        String str3 = "RSTUVWXYZ?!";
+        String str4 = "0123";
+        String str5 = "456789_@$#&";
+        List<String> testInputList = new ArrayList<String>(Arrays.asList(str1, str2, str3, str4, str5));
+        Set<String> actualResultSet = lcsProcessor.getLongestCommonSubstrings(testInputList);
+
+        assertEquals(testResultSet.size(), actualResultSet.size());
+        assertTrue(actualResultSet.contains("RSTUVWXYZ?!"));
+        assertTrue(actualResultSet.contains("456789_@$#&"));
     }
 
     @Test
@@ -62,7 +130,7 @@ public class LcsProcessorTest {
         testSet.add("Humungous");
         testSet.add("The Entire Universe");
         testSet.add("Biggest Of All Time");
-        Set<String> resultSet = lcsProcessor.retainMax(testSet);
+        Set<String> resultSet = lcsProcessor.retainMax(testSet, 0);
 
         assertEquals(2, resultSet.size());
         assertTrue(resultSet.contains("The Entire Universe"));
